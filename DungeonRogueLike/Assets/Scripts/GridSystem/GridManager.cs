a6d606846a742bc5fc2807a3bd1361d14cc49602
Bob Hoogenboom <bhoogenboom23@gmail.com>
2024-02-12 16:00:02 +0100

    grid manager update
    added grid manager script
    added grid node script

---
+++ DungeonRogueLike/Assets/Scripts/GridSystem/GridManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary>
+/// Source: https://www.youtube.com/watch?v=4JaHSLA2CKs
+/// 
+/// This script keeps track of the Grid and all entities on the grid like Enemies, player(s) [and pick-ups*]
+/// *not sure about pick ups yet but we'll see
+/// </summary>
+public class GridManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ DungeonRogueLike/Assets/Scripts/GridSystem/GridNode.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridNode 
+{
+    public Vector2Int cords;
+
+    public GridNode(Vector2Int cords)
+    {
+        this.cords = cords;
+    }
+}
